99c1908cf736ba2df52a74db530272454315171c
SongGayoung <xjongam1x@gmail.com>
2023-04-20 13:47:01 +0900

    created: modal

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        [{ id: "c1", name: "SWEATSHIRT_NAVY", amount: 2, price: "80,100" }].map(
+          (item) => <li>{item.name}</li>
+        ),
+      ]}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>결제금액</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>닫기</button>
+        <button className={classes.button}>주문하기</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
