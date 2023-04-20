a6a0e438be74dbf03dfc417bb53de4133c590b80
SongGayoung <xjongam1x@gmail.com>
2023-04-20 11:58:05 +0900

    created: HeaderCartButton component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import MarkMmain from "../../assets/Markm.png";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>MarkM</h1>
-        <button>장바구니</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={MarkMmain} alt="MarkM model is Mimi" />
