6d4e5795627f89e1c501dddd236f764f133ce749
SongGayoung <xjongam1x@gmail.com>
2023-04-20 13:14:38 +0900

    created: Input component

--- src/components/Clothes/ClothItem/ClothItemFrom.jsx
+++ src/components/Clothes/ClothItem/ClothItemFrom.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import classes from "./ClothItemFrom.module.css";
+import Input from "../../UI/Input";
 
 const ClothItemFrom = (props) => {
   return (
     <form className={classes.form}>
-      <input />
+      <Input
+        label="수량"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ 추가</button>
     </form>
   );
