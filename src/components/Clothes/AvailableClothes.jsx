f25c90ca028a9e9d27dfa34ea55c4451c6f50ea2
SongGayoung <xjongam1x@gmail.com>
2023-04-21 12:32:01 +0900

    modified: cart logic

--- src/components/Clothes/AvailableClothes.jsx
+++ src/components/Clothes/AvailableClothes.jsx
@@ -6,26 +6,26 @@ import ClothItem from "./ClothItem/ClothItem";
 const DUMMY_CLOTHES = [
   {
     id: "m1",
-    name: "SWEATSHIRT_NAVY",
+    name: "SweatShirt",
     description: "[23S/S] [GOLF] LOGO SET-UP RUGBY SWEATSHIRT_NAVY (MGIBA8712)",
     price: 80100,
   },
   {
     id: "m2",
-    name: "CROP SWEATSHIRTS_IVORY",
+    name: "Crop SweatShirt",
     description:
       "[23S/S] (WOMEN) FLOWER PRINT CROP SWEATSHIRTS_IVORY (MGIBA8763)",
     price: 71100,
   },
   {
     id: "m3",
-    name: "T-SHIRTS_GREEN",
+    name: "Long Sleeve",
     description: "[23S/S] STRIPE LONG SLEEVE T-SHIRTS_GREEN (MGIBB8733)",
     price: 53100,
   },
   {
     id: "m4",
-    name: "HOODIE_GREY MELANGE",
+    name: "Hoodie",
     description: "[23S/S] CAT LOGO GRAPHIC HOODIE_GREY MELANGE (MGIBA8606)",
     price: 80100,
   },
