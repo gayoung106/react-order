75bd3f1db534f39ba387f35b52ae68752c03b306
SongGayoung <xjongam1x@gmail.com>
2023-04-21 12:18:49 +0900

    created: CartItem component & added: cart logic

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,27 @@
 import { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
   const cartCTX = useContext(CartContext);
 
   const totalAmount = `${cartCTX.totalAmount.toLocaleString()}원`;
-
+  const hasItems = cartCTX.items.length > 0;
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCTX.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -26,7 +36,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]} onClick={props.onClose}>
           닫기
         </button>
-        <button className={classes.button}>주문하기</button>
+        {hasItems && <button className={classes.button}>주문하기</button>}
       </div>
     </Modal>
   );
